c0356b2fbf9eab64a4908a24499073fea0251f8e
VirTondon <86696898+VirTondon@users.noreply.github.com>
2022-08-16 15:08:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ function gotResults(error, results) {
     console.error(error);
   } else {
     console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    
+    console.log(results);
+    img = document.getElementById('animal_image');
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    
+    if (results[0].label == "Barking") {
+      img.src = 'bark copy.gif';
+      dog = dog+1;
+    } else if (results[0].label == "meowing") {
+      img.src = 'meow copy.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen copy.gif';
+    }
+  }
+}
